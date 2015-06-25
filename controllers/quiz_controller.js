278eeddcc73112fba9c386ed7fd8e3fddbd7f7ff
Adrian <aqueipo@gmail.com>
2015-06-25 14:28:40 +0200

    Fixed bug in the controller

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -10,11 +10,13 @@ exports.question = function (req, res) {
 
 // GET /quizes/answer
 exports.answer = function (req, res) {
-	if (req.query.respuesta === quiz[0].respuesta) {
-		res.render('quizes/answer', {respuesta: 'Correcto'});
-	} else {
-		res.render('quizes/answer', {respuesta: 'Incorrecto'});
-	}
+	models.Quiz.findAll().success(function (quiz) {
+		if (req.query.respuesta === quiz[0].respuesta) {
+			res.render('quizes/answer', {respuesta: 'Correcto'});
+		} else {
+			res.render('quizes/answer', {respuesta: 'Incorrecto'});
+		}
+	})
 };
 
 // GET /author
